0f6ed319fc21f5b921d0aec52207d3f15c526f75
unchaptered <workstation19961002@gmail.com>
2024-06-16 16:29:13 +0900

    feat : basic gin server

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+    router.GET("/", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{"message": "Welcome to the Bookstore API!"})
+    })
+	router.Run(":8080")
+}
